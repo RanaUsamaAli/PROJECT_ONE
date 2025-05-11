15fe7c19259381b34d2453bd2e007262fa665a31
RanaUsamaAli <86379116+RanaUsamaAli@users.noreply.github.com>
2025-05-12 01:37:33 +0500

    Add files via upload

---
+++ Code_In_Pk/script/main.js
@@ -0,0 +1,9 @@
+// Nav Bar Scroll Funcation
+   window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
